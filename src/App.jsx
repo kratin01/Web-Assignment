639a6f669ebb1b23b7d87bf9d44dfa32ab400d24
kratin01 <kratinaggarwal8750@gmail.com>
2024-10-03 16:25:04 +0530

    feat: Add initial project files and configurations
    - Add tailwindcss base, components, and utilities to index.css
    - Configure postcss with tailwindcss and autoprefixer in postcss.config.js
    - Configure Vite with react plugin in vite.config.js
    - Configure tailwindcss content and theme in tailwind.config.js
    - Create App component with a title in App.jsx
    - Create main.jsx to render the App component with strict mode
    - Add gitignore rules for logs, node_modules, and editor files
    - Create index.html with basic structure and script tag for main.jsx
    - Update README.md with project description and plugin information
    - Update package.json with dependencies, devDependencies, and scripts
    - Configure ESLint with recommended rules and react plugins in eslint.config.js
    - Add Vite logo in public/vite.svg

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline flex justify-center mt-5">Web Assignment</h1>
+    </>
+  );
+}
+
+export default App;
