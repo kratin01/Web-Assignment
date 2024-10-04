73c13be6080c0f668e8e6ea7ce2d61c94978f1f7
kratin01 <kratinaggarwal8750@gmail.com>
2024-10-04 10:30:49 +0530

    Add New Components and some packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import axios from "axios";
+import Card from "./components/Card";
 
 
 function App() {
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(10);
+
+  useEffect(()=>{
+    const fetchPost = async () => {
+      setLoading(true);
+      const res = await axios.get('https://webteam-assignments.onrender.com/events');
+      setPost(res.data);
+      setLoading(false);
+    }
+    fetchPost();
+  },[]);
+
+  console.log(post);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline flex justify-center mt-5">Web Assignment</h1>
+      <Card />
     </>
   );
 }
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PostCard = ({post,loading}) => {
+    if(loading){
+        return <h2>Loading...</h2>
+    }
+  return <ul>
+    
+  </ul>
+}
+
+export default PostCard
